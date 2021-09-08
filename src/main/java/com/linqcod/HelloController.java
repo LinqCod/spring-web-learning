f185a4b0e8974607451845d392435f33ccf60c40
linqcod <linqcod@yandex.ru>
2021-09-08 12:00:56 -0700

    hello-world-xml-config

---
+++ src/main/java/com/linqcod/HelloController.java
@@ -0,0 +1,14 @@
+package com.linqcod;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+
+}
